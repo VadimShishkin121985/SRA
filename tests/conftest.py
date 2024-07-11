074464ed473db5b1f4be480afce2616ceb6a7117
Vadim <121985ops@gmail.com>
2024-07-11 11:05:02 +0300

    aded yml file

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ def chrome(request):
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_experimental_option('prefs', {'credentials_enable_service': False, 'profile.password_manager_enabled': False})
+    options.add_argument('--headless')
     options.add_argument('--disable-cache')
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-infobars')
