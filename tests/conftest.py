9b9e0747918049b74772cc86d49829c5f75c17fd
Vadim <121985ops@gmail.com>
2025-01-17 19:44:59 +0200

    comit15

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ def chrome(request):
     #options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-web-security')
     options.add_argument('--allow-running-insecure-content')
     options.add_argument('--window-size=1920,1080')
