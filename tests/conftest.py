38cc3c6521c41fdc5573ae8c071218263452aaf7
Vadim <121985ops@gmail.com>
2025-01-24 17:58:29 +0200

    comit18

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,106 @@
 import pytest
 from selenium import webdriver
-import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='class')
 def chrome(request):
-    # Устанавливаем нужную версию ChromeDriver
-    chromedriver_autoinstaller.install()
+    # Настраиваем ChromeOptions
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")  # Новый режим headless
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-browser-side-navigation")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--start-maximized")
 
-    # Настройка опций для Chrome
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_experimental_option('prefs',
-                                    {'credentials_enable_service': False, 'profile.password_manager_enabled': False})
-    options.add_argument('--disable-cache')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--disable-infobars')
-    options.add_argument('--disable-browser-side-navigation')
-    #options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')
-    options.add_argument('--disable-web-security')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument('--window-size=1920,1080')
-
-    # Создание экземпляра ChromeDriver
+    # Создаем сервис с явным указанием пути к ChromeDriver
     service = Service()
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.set_page_load_timeout(330)
+    
+    # Инициализируем драйвер с сервисом и опциями
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.set_page_load_timeout(30)
+    driver.implicitly_wait(10)  # Добавляем неявное ожидание
 
-    # Привязка драйвера к классу, если он существует
     if request.cls:
         request.cls.driver = driver
-
     yield driver
-
-    # Завершение работы драйвера после тестов
     driver.quit()
+
+
+@pytest.fixture(scope='class')
+def firefox(request):
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
+    driver.set_page_load_timeout(30)
+    if request.cls:
+        request.cls.driver = driver
+    yield driver
+    driver.quit()
+
+
+
+
+
+#
+#
+#
+# @pytest.fixture(scope='class')
+# def chrome(request):
+#     options = webdriver.ChromeOptions()
+#     options.add_experimental_option('excludeSwitches', ['enable-automation'])
+#     options.add_experimental_option('useAutomationExtension', False)
+#     options.add_experimental_option('prefs',
+#                                     {'credentials_enable_service': False, 'profile.password_manager_enabled': False})
+#     options.add_argument('--disable-cache')
+#     options.add_argument('--disable-extensions')
+#     options.add_argument('--disable-infobars')
+#     options.add_argument('--disable-browser-side-navigation')
+#     options.add_argument('--disable-gpu')
+#     options.add_argument('--no-sandbox')
+#     options.add_argument('--disable-dev-shm-usage')
+#     options.add_argument('--headless')
+#     options.add_argument('--disable-web-security')
+#     options.add_argument('--allow-running-insecure-content')
+#     options.add_argument('--window-size=1920,1080')
+#
+#     try:
+#         driver = webdriver.Chrome(options=options)
+#         driver.maximize_window()
+#         driver.set_page_load_timeout(120)
+#         driver.implicitly_wait(60)
+#
+#         if request.cls:
+#             request.cls.driver = driver
+#
+#         yield driver
+#
+#     finally:
+#         if 'driver' in locals():
+#             driver.quit()
+#
+#
+# @pytest.fixture(scope="function")
+# def browser(request):
+#     options = webdriver.ChromeOptions()
+#     driver = webdriver.Chrome(options=options)
+#
+#     yield driver
+#
+#     # Делаем скриншот только если тест провалился
+#     if hasattr(request.node, "rep_call") and request.node.rep_call.failed:
+#         driver.save_screenshot(f"screenshot_{request.node.name}.png")
+#
+#     driver.quit()
+#
+#
+# # Хук для сохранения результата теста
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
+#    setattr(item, "rep_" + rep.when, rep)
