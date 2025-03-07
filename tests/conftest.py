2e35376aa898e76e9002f869c5df414af510cef2
Vadim <121985ops@gmail.com>
2025-03-07 11:48:31 +0200

    comit30

--- tests/conftest.py
+++ tests/conftest.py
@@ -111,8 +111,8 @@ def pytest_runtest_makereport(item, call):
 
     # Если тест зафейлился, добавляем задержку
     if rep.when == "call" and rep.failed:
-        print(f"Test {item.name} failed. Waiting for 60 seconds before proceeding...")
-        time.sleep(600)
+        print(f"Test {item.name} failed. Waiting for 700 seconds before proceeding...")
+        time.sleep(700)
 
 
 def pytest_configure(config):
