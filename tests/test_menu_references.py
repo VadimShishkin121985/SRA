b0701d7c9119a3d6de9cd3666644f8a59d8c3ec7
Vadim <121985ops@gmail.com>
2026-03-27 17:14:23 +0200

    separated tests 1

--- tests/test_menu_references.py
+++ tests/test_menu_references.py
@@ -1,7 +1,4 @@
-
-
 import pytest
-
 
 from pages.menu_pages.menu_tools import MenuTools
 from pages.menu_pages.menu_services import MenuServices
@@ -9,11 +6,15 @@ from pages.menu_pages.menu_references import MenuReferences
 from pages.menu_pages.menu_company import MenuCompany
 from pages.menu_pages.footer_page import MenuFooter
 
+
 @pytest.mark.browser_scope('function')
 @pytest.mark.usefixtures('chrome')
-class TestReferencesMenu:
+class TestMenu:
     _class_cleanup = False
 
+    # =========================================================
+    # 🔧 PAGE OBJECTS (lazy init)
+    # =========================================================
     @property
     def page_menu_tools(self):
         if not hasattr(self, '_page_menu_tools'):
@@ -44,6 +45,9 @@ class TestReferencesMenu:
             self._page_footer = MenuFooter(self.driver).open()
         return self._page_footer
 
+    # =========================================================
+    # 📚 REFERENCES
+    # =========================================================
     def test_references_world_sea_ports(self):
         self.page_menu_references.go_to_world_sea_ports_page()
 
@@ -110,3 +114,93 @@ class TestReferencesMenu:
     def test_references_truck_type(self):
         self.page_menu_references.go_to_truck_type_page()
 
+    # =========================================================
+    # 🚢 SERVICES
+    # =========================================================
+    def test_services_request_quote(self):
+        self.page_menu_services.go_to_request_a_quote_page()
+
+    def test_services_active_shipping_leads(self):
+        self.page_menu_services.go_to_all_shipping_leads_page()
+
+    def test_services_logistic_service_by_country(self):
+        self.page_menu_services.go_to_logistics_services_page()
+
+    def test_services_for_carrier_forwarder(self):
+        self.page_menu_services.go_to_carrier_forwarder_page()
+
+    def test_services_for_importers_exporters(self):
+        self.page_menu_services.go_to_importers_page()
+
+    def test_services_fcl_shipping(self):
+        self.page_menu_services.go_to_fcl_shipping_services_page()
+
+    def test_services_lcl_shipping(self):
+        self.page_menu_services.go_to_lcl_shipping_services_page()
+
+    def test_services_bulk_and_break_bulk(self):
+        self.page_menu_services.go_to_bulk_services_page()
+
+    def test_services_dangerous_cargoes(self):
+        self.page_menu_services.go_to_dangerous_cargoes_services_page()
+
+    def test_services_insurance(self):
+        self.page_menu_services.go_to_insurance_services_page()
+
+    def test_services_inspection_services(self):
+        self.page_menu_services.go_to_inspection_services_page()
+
+    def test_services_abandoned_cargoes(self):
+        self.page_menu_services.go_to_abandoned_cargoes_page()
+
+    def test_services_certification(self):
+        self.page_menu_services.go_to_certification_services_page()
+
+    def test_services_project_cargo(self):
+        self.page_menu_services.go_to_project_cargo_services_page()
+
+    def test_services_customs_clearance(self):
+        self.page_menu_services.go_to_customs_clearance_services_page()
+
+    def test_services_survey_services(self):
+        self.page_menu_services.go_to_survey_services_page()
+
+    def test_services_reefer_services(self):
+        self.page_menu_services.go_to_reefer_services_page()
+
+    def test_services_warehousing(self):
+        self.page_menu_services.go_to_warehousing_services_page()
+
+    def test_services_10_foot_containers(self):
+        self.page_menu_services.go_to_test_10_foot_containers_page()
+
+    def test_services_pharmaceutical_logistics(self):
+        self.page_menu_services.go_to_pharmaceutical_logistics_page()
+
+    def test_services_vehicle_automotive(self):
+        self.page_menu_services.go_to_vehicle_automotive_shipping()
+
+    # =========================================================
+    # 🏢 COMPANY
+    # =========================================================
+    def test_company_contact_us(self):
+        self.page_menu_company.go_to_contact_us_page()
+
+    def test_company_about_us(self):
+        self.page_menu_company.go_to_about_us_page()
+
+    def test_company_faqs(self):
+        self.page_menu_company.go_to_faqs_page()
+
+    def test_company_blog(self):
+        self.page_menu_company.go_to_blog_page()
+
+    def test_company_blog_post(self):
+        self.page_menu_company.go_to_blog_post()
+
+    # =========================================================
+    # 🧹 CLEANUP
+    # =========================================================
+    @classmethod
+    def teardown_class(cls):
+        cls._class_cleanup = True
